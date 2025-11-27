25963ea192267ef2e9a7acab481fa7ac01ed5ed3
medericc <bleublanctour1@>
2025-11-27 22:05:47 +0100

    m

---
+++ app/autre.tsx
@@ -0,0 +1,37 @@
+
+// "barbe-albert": {
+//   "id": "barbe-albert",
+//   "name": "Barbe Alberte d’Ernécourt",
+ 
+//   "category": "",
+
+//   "period": "17e siècle",
+//   "country": "France",
+//   "type": "noble",
+//   "birthDate": "1607",
+//   "deathDate": "1660",
+//   "feastDay": "",
+//   "image": "/saints/barbe.png",
+//   "cover": "/saints/barbe-cover.jpg",
+//   "shortDescription": "Noël Pinot devint prêtre en 1770 et servit les plus modestes. Refusant la Constitution civile du clergé, il poursuivit son ministère clandestinement. Arrêté en 1794, il fut guillotiné à Angers, montant à l’échafaud en habits sacerdotaux en récitant « Introibo ad altare Dei ».",
+//   "biography": {
+
+//   "sa-jeunesse": " Barbe Alberte d’Ernécourt, né le 14 mai 1607 et est dame de Gibeaumeix et de Neuville-en-Verdunois. Elle est élevée par sa tante et marraine, Barbe d’Ernécourt (épouse de Varin de Nyvenheim), au château d’Étrepy. Le 29 février 1624. Elle épouse Jean-Jacques de Haraucourt, seigneur de Saint-Balmont.",
+
+//   "ses-debuts": "En 1633, quand son mari, colonel d'infanterie, rejoint le duc de Lorraine Charles IV et combat en Allemagne auprès des Impériaux (notamment pour fuir les représailles de Louis XIII après le siège de Nancy), laissant le Barrois ravagé par l’occupation française, Alberte-Barbe de Saint-Balmont demeure sur ses terres et entreprend de transformer son fief en une véritable forteresse autonome.",
+
+//   "ses-actions": " Elle fait réparer les remparts, constitue une milice disciplinée en armant et exerçant au mousquet les paysans réfugiés chez elle, et s’appuie sur sa splendide écurie pour bâtir une petite armée efficace. Autour d’elle, son domaine devient un véritable État miniature : elle y héberge parfois jusqu'à 400 gentilshommes en attente du service du Roi, ainsi que les paysans des environs et les meilleurs artisans de Bar, qu’elle autorise à travailler en paix et sans impôts, à condition qu’ils prennent les armes si la situation l’exige."
+//  ,
+
+
+
+//  "sa-malice": "Les troupes françaises sont repoussées assez vite, mais la menace persiste : après la défaite française de Mayence en 1635, des bandes de mercenaires lorrains, impériaux et surtout des « Cravates » abandonnés par leurs chefs s’engouffrent dans le pays, pillent, rançonnent et terrorisent la population. Alberte-Barbe, portant le chapeau à plumes, le justaucorps et montant à cheval comme un cavalier, met alors en pratique sa science de la guerre. Elle use de ruse, interceptant parfois le courrier des marchands pour découvrir les mouvements de l'ennemi, et sa petite armée, par des coups de main audacieux (comme lorsqu'elle récupère seule ses chevaux volés par une cinquantaine de cavaliers), des feintes savamment orchestrées et des charges fulgurantes, parvient à tenir ces ennemis en respect et finit même par les effrayer."
+ 
+ 
+ 
+//  ,"son-independance": "Elle secourt tour à tour Bar et Verdun, escorte les convois de vivres, capture des prisonniers qu'elle livre aux Français, contribue à la sûreté des routes et s’attire l’admiration du marquis de Feuquières, gouverneur de Verdun. Impressionné par son savoir militaire lors d'une visite, ce dernier demande au roi de placer sous son commandement une unité stationnée à Neuville et lui propose même un régiment royal, offre qu’elle refuse, préférant commander ses propres vassaux et restant jalouse de son indépendance autant que de son honneur. En 1650, malgré sa fidélité à la France, le maréchal de La Ferté ordonne la réquisition de ses biens ; elle se retire alors au couvent des clarisses de Bar-le-Duc, où elle mourra, laissant l’image d’une femme « qui aime autant prier Dieu que se battre, aussi dévote que vaillante »."
+
+
+
+// }
+
